71dea9d61816afd3c1b4c762fcc3d1779988823a
lijunjie2232 <2533584225@qq.com>
2025-06-01 12:06:43 +0900

    blog theme

--- themes/particlex/source/js/lib/home.js
+++ themes/particlex/source/js/lib/home.js
@@ -5,6 +5,30 @@ mixins.home = {
         let id = Math.floor(Math.random() * images.length);
         background.style.backgroundImage = `url('${images[id]}')`;
         this.menuColor = true;
+
+        // Apply blur effect based on scroll
+        const homePostsWrap = this.$refs.homePostsWrap;
+
+        const maxBlur = 10; // Max blur: 10px
+        const startBlurOffset = 0.618;
+        if (homePostsWrap && background) {
+            window.addEventListener("scroll", () => {
+                const rect = homePostsWrap.getBoundingClientRect();
+                const windowHeight = window.innerHeight * startBlurOffset; // Height of the viewport
+                offsetFromTop = 0;
+                if (rect.top < windowHeight) {
+                    offsetFromTop = Math.floor((windowHeight - rect.top) / windowHeight * maxBlur); // How much it's scrolled past the top of the viewport}
+                }
+
+                // Blur increases as homePostsWrap reaches the top
+                let blurValue = Math.min(offsetFromTop, maxBlur);
+                background.style.filter = `blur(${blurValue}px)`;
+                console.log(offsetFromTop);
+                console.log(rect.top);
+            });
+        }
+        console.log(homePostsWrap);
+        console.log(background);
     },
     methods: {
         homeClick() {
